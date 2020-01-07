9962df431951a737f5d26b6ba353c553c20f647e
Soner Mezgitci <you@example.com>
2020-01-07 14:04:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+let name = 'Sylwia';
+newHeader.innerHTML = `${name} is the champion!`;
+newHeader.className = 'victory';
+
+
+
+
+
+
