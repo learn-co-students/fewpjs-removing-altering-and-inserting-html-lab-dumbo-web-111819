0d7737807d81b8c0305cc8221dcaec0fbeba45c1
Baruch Phillips <baruchphillips@gmail.com>
2020-01-07 10:24:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+// remove id of main
+main.remove()
+let newHeader = document.createElement('h1')
+// document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.innerHTML = 'BP is the champion'
